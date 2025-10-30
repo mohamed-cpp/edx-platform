e2758aa3121c3ef0d9a4d78a3ab591cb4260e43c
salman2013 <salman.nawaz@arbisoft.com>
2025-10-30 18:36:53 +0500

    fix: fix test

--- lms/djangoapps/courseware/tests/test_video_mongo.py
+++ lms/djangoapps/courseware/tests/test_video_mongo.py
@@ -84,7 +84,7 @@ class TestVideoYouTube(TestVideo):  # lint-amnesty, pylint: disable=missing-clas
     @patch('xblock.utils.resources.ResourceLoader.render_django_template', side_effect=mock_render_template)
     def test_video_constructor(self, mock_render_django_template):
         """Make sure that all parameters extracted correctly from xml"""
-        context = self.block.student_view(None).content
+        self.block.student_view(None)
         sources = ['example.mp4', 'example.webm']
 
         expected_context = {
@@ -146,11 +146,22 @@ class TestVideoYouTube(TestVideo):  # lint-amnesty, pylint: disable=missing-clas
             'video_id': '',
         }
 
-        # Render expected context using the same mock to get comparable output
-        expected_content = mock_render_template('templates/video.html', expected_context)
+        # Verify the mock was called
+        mock_render_django_template.assert_called_once()
         
-        # Compare using the existing helper that handles user_id omission
-        assert get_context_dict_from_string(context) == get_context_dict_from_string(expected_content)
+        # Get the actual context that was passed to render_django_template
+        call_args = mock_render_django_template.call_args
+        # actual_template = call_args[0][0]  # First positional arg is template path
+        actual_context = call_args[0][1]  # Second positional arg is context
+        
+        # # Verify correct template was used
+        # assert actual_template == 'templates/video.html'
+        
+        # Verify context matches expected
+        # Note: i18n_service is passed as kwarg, so we just verify the context dict
+        # assert actual_context == expected_context
+        assert get_context_dict_from_string(str(actual_context)) == \
+       get_context_dict_from_string(str(expected_context))
 
 
 class TestVideoNonYouTube(TestVideo):  # pylint: disable=test-inherits-tests
@@ -238,14 +249,21 @@ class TestVideoNonYouTube(TestVideo):  # pylint: disable=test-inherits-tests
             'video_id': '',
         }
 
-        # Render expected context using the same mock to get comparable output
-        expected_content = mock_render_template('templates/video.html', expected_context)
+        # Verify the mock was called
+        mock_render_django_template.assert_called_once()
         
-        # Compare using the existing helper that handles user_id omission
-        expected_result = get_context_dict_from_string(expected_content)
-        assert get_context_dict_from_string(context) == expected_result
-        assert expected_result['download_video_link'] == 'example.mp4'
-        assert expected_result['display_name'] == 'A Name'
+        # Get the actual context that was passed to render_django_template
+        call_args = mock_render_django_template.call_args
+        actual_template = call_args[0][0]  # First positional arg is template path
+        actual_context = call_args[0][1]  # Second positional arg is context
+        
+        # Verify correct template was used
+        assert actual_template == 'templates/video.html'
+        
+        # Verify context matches expected
+        assert actual_context == expected_context
+        assert actual_context['download_video_link'] == 'example.mp4'
+        assert actual_context['display_name'] == 'A Name'
 
 
 @ddt.ddt
